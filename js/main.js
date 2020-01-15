3044e2b42d7f19ef642f3298a467de145e651bfb
jobtalle <jobtalle@hotmail.com>
2020-01-15 13:16:16 +0100

    Removed ugly gradient

--- js/main.js
+++ js/main.js
@@ -6,12 +6,7 @@ const gradients = [
         0.1, new Color(.2, .2, .27, 1),
         0.4, new Color(1, 1, 0.6, 1),
         0.7, new Color(1, 1, 1, 1),
-        1, new Color(0.7, 0.7, 0.7, 1)),
-    new Gradient(
-        0, new Color(0, 0, 0, 1),
-        0.5, new Color(0, 1, 1, 1),
-        1, new Color(1, 1, 1, 1)
-    )
+        1, new Color(0.7, 0.7, 0.7, 1))
 ];
 const wrapper = document.getElementById("wrapper");
 const canvas = document.getElementById("renderer");
